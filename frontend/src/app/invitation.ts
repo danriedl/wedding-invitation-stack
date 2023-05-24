e28c8e27ab54fbd81560e90786ee143e29b75dad
Daniel Riedl <amkoenigsholz9@gmail.com>
2023-05-24 20:49:13 +0200

    feat(Frontend): Additionial check for further guests.

--- frontend/src/app/invitation.ts
+++ frontend/src/app/invitation.ts
@@ -1,9 +1,10 @@
 export interface Invitation {
-    uuid?: string
-    name?: string
-    accepted?: boolean
-    rideshare?: 1 | 2 | 3 | 4
-    further_guests: string[]
-    need_to_know?: string
-    favourite_song?: string
-  }
+  uuid?: string;
+  name?: string;
+  accepted?: boolean;
+  rideshare?: 1 | 2 | 3 | 4;
+  further_guests: string[];
+  need_to_know?: string;
+  favourite_song?: string;
+  further_guest?: string;
+}
